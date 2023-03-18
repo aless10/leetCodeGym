6e2209c1870f4005a45f38fd2bb3e8c9e51c3cd0
Alessio Izzo <alessio.izzo86@gmail.com>
2023-03-18 16:19:52 +0100

    improve two sums in go

--- twoSum/two_sum.go
+++ twoSum/two_sum.go
@@ -1,23 +1,11 @@
 func twoSum(nums []int, target int) []int {
-	d := make(map[int][2]int)
+	d := make(map[int]int)
 	for i, v := range nums {
-			value, ok := d[v]
-			if (ok && value[0] * 2 == target) {
-					return []int{value[1], i}
-			}
-			d[v] = [2]int{target - v, i}
-	}
-	
-	for _, v := range d {
-			_, ok := d[v[0]]
-
+			value, ok := d[target - v]
 			if ok {
-					complement_index := d[v[0]][1]
-					if complement_index != v[1] {
-							return []int{v[1], complement_index}
-					}
+					return []int{value, i}
 			}
+			d[v] = i
 	}
-
 	return []int{0, 0}
 }
